627c73eff2d16473bff00805544479973e785f2f
Rafael Gamez Diaz <gamezr@zippyttech.com>
2020-02-04 10:17:51 -0400

    Updating status default in migration

---
+++ database/migrations/2020_02_04_133512_update_status_default_value.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Models\Subscription;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateStatusDefaultValue extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('subscriptions', function (Blueprint $table) {
+            $table->string('status')->default(Subscription::SUBSCRIPTION_ACTIVE)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
